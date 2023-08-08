f6c4b34349359f399d5a8cf33ed689f469335df4
Felix-Nilsson <felix.nilsson.se@gmail.com>
2023-08-08 23:49:06 +0200

    :sparkles: a small version of a 1 'neuron' model that approximates a weight

--- c/main.c
+++ c/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+Goal: adjust a number to approach the weight of the training set
+i.e. our training set is x * w = y, and the true w = 2
+
+*/
+
 float train[][2] = {
   {0,0},
   {1,2},
@@ -17,24 +23,42 @@ float rand_float(void){
   return (float) rand() / (float) RAND_MAX;
 }
 
+float cost(float w){
+  float result = 0.0f; 
+
+  for(size_t i = 0; i < train_count; ++i){
+    float x = train[i][0];
+    float y = x * w;
+    float d = y - train[i][1];
+    result += d*d;
+
+  }
+
+  result /= train_count;
+
+  return result;
+
+}
+
 int main(){
 
   //srand(time(0));
   srand(70);
   float w = rand_float()*10.0f;
-  printf("%f\n",w);
+  float rate = 1e-3;
+  float eps = 1e-3;
 
+  printf("%f\n",cost(w));
 
-  float result = 0.0f;
-  for(size_t i = 0; i < train_count; ++i){
-    float x = train[i][0];
-    float y = x*w;
-    float d = y - train[1][1];
-    result += d*d;
-    printf("distance: %f\n", d);
+  for(size_t i = 0; i < 500; ++i){
+    float dcost = (cost(w + eps) - cost(w))/eps;
+    w -= rate*dcost;
+    printf("cost = %f, w= %f\n", cost(w),w);
 
   }
-
   
+  printf("------------------------------\n");
+  printf("%f\n",w);
+
   return 0;
 }
