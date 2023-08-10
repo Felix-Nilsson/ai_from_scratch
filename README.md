9d74f3e71086f67965c6df1c8127f11ccb30453a
Felix-Nilsson <felix.nilsson.se@gmail.com>
2023-08-10 22:35:52 +0200

    added bias to the model

---
+++ README.md
@@ -0,0 +1,3 @@
+Some implementations of concepts in ML in C.
+
+based on [this](https://www.youtube.com/watch?v=PGSba51aRYU&t=1505s) video series by tsoding
--- c/main.c
+++ c/main.c
@@ -23,12 +23,12 @@ float rand_float(void){
   return (float) rand() / (float) RAND_MAX;
 }
 
-float cost(float w){
+float cost(float w, float b){
   float result = 0.0f; 
 
   for(size_t i = 0; i < train_count; ++i){
     float x = train[i][0];
-    float y = x * w;
+    float y = x * w + b;
     float d = y - train[i][1];
     result += d*d;
 
@@ -43,22 +43,30 @@ float cost(float w){
 int main(){
 
   //srand(time(0));
-  srand(70);
+  srand(time(0));
   float w = rand_float()*10.0f;
+  float b = rand_float()*5.0f;
+
   float rate = 1e-3;
   float eps = 1e-3;
 
-  printf("%f\n",cost(w));
+  
 
   for(size_t i = 0; i < 500; ++i){
-    float dcost = (cost(w + eps) - cost(w))/eps;
-    w -= rate*dcost;
-    printf("cost = %f, w= %f\n", cost(w),w);
+    float c = cost(w,b);
+
+    float dw = (cost(w + eps, b) - c)/eps;
+    float db = (cost(w, b + eps) - c)/eps;
+
+    w -= rate*dw;
+    b -= rate*db;
+
+    printf("cost = %f, w= %f, b= %f\n", c,w,b);
 
   }
   
   printf("------------------------------\n");
-  printf("%f\n",w);
+  printf("w= %f, b= %f\n",w,b);
 
   return 0;
 }
